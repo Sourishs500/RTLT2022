fcb57d8c0b2baf3bbee593b6d351d9bde8fe8443
Sourishs500 <31145059+Sourishs500@users.noreply.github.com>
2022-11-30 00:15:29 -0800

    Update main.c

--- main.c
+++ main.c
@@ -73,7 +73,7 @@
 #define LED_BLINK_INTERVAL 200
 
 // New variables added for Final Baseline Project below
-#define ANGLE_MAG_MAX_THRESHOLD 30
+#define ANGLE_MAG_MAX_THRESHOLD 25
 #define MAX_ROTATION_ACQUIRE_CYCLES 400
 
 //#define NOT_DEBUGGING
@@ -560,7 +560,202 @@ void Feature_Extraction_State_1(void *handle_g, int * ttt_1, int * ttt_2,
 
 }
 
+void Feature_Extraction_State_2(void *handle_g, int * ttt_1, int * ttt_2,
+		int * ttt_3, int * ttt_mag_scale) {
 
+	int ttt[3], ttt_state_0[3], ttt_initial[3], ttt_offset[3];
+	char msg1[128];
+	int axis_index, sample_index;
+	float rotate_angle[3];
+	float angle_mag, angle_mag_max_threshold;
+	float Tsample;
+
+	/*
+	* Assign the initial values provided by execution of State 0 in
+	* ttt_1 and ttt_2
+	*/
+	ttt_state_0[0] = *ttt_1;
+	ttt_state_0[1] = *ttt_2;
+
+	angle_mag_max_threshold = ANGLE_MAG_MAX_THRESHOLD;
+
+	/*
+	* Compute sample period with scaling from milliseconds
+	* to seconds
+	*/
+
+	Tsample = (float)(DATA_PERIOD_MS)/1000;
+
+   /*
+	* Initialize rotation angle values
+	*/
+
+	for (axis_index = 0; axis_index < 3; axis_index++) {
+		ttt[axis_index] = 0;
+		rotate_angle[axis_index] = 0;
+	}
+
+	/*
+	* Rotation Rate Signal integration loop
+	*
+	* Note that loop cycle time is DATA_PERIOD_MS matching the SensorTile
+	* sensor sampling period
+	*
+	* Permit integration loop to operate no longer than a maximum
+	* number of cycles, MAX_ROTATION_ACQUIRE_CYCLES. Note: This sets
+	* maximum acquisition time to be MAX_ROTATION_ACQUIRE_CYCLES*Tsample
+	*
+	* For 4 second delay, apply MAX_ROTATION_ACQUIRE_CYCLES 400
+	*
+	*/
+	/*
+	* Acquire Rotation Rate values prior to motion
+	*
+	* This includes the initial sensor offset value to be subtracted
+	* from all subsequent samples
+	*/
+
+	getAngularVelocity(handle_g, ttt_offset);
+
+	/*
+	 * Notify user to initiate motion
+	 */
+
+	sprintf(msg1, "\r\nStart Third State Motion to New Orientation when LED On");
+	CDC_Fill_Buffer((uint8_t *) msg1, strlen(msg1));
+	BSP_LED_On(LED1);
+
+	for(sample_index = 0; sample_index < MAX_ROTATION_ACQUIRE_CYCLES; sample_index++) {
+
+		/*
+		* Acquire initial sample value of rotation rate and remove
+		* offset value
+		*/
+
+		for(axis_index = 0; axis_index < 3; axis_index++) {
+			ttt_initial[axis_index] = ttt[axis_index] - ttt_offset[axis_index];
+		}
+		/*
+		* Introduce integration time period delay
+		*/
+		HAL_Delay(DATA_PERIOD_MS);
+
+		/*
+		* Acquire current sample value of rotation rate and remove
+		* offset value
+		*/
+
+		getAngularVelocity(handle_g, ttt);
+		for(axis_index = 0; axis_index < 3; axis_index++){
+			ttt[axis_index] = ttt[axis_index] - ttt_offset[axis_index];
+		}
+
+		/*
+		* Suppress value of Z-Axis rotation signals
+		*/
+
+		ttt_initial[2] = 0;
+		ttt[2] = 0;
+
+		/*
+		* Compute rotation angles by integration
+		*/
+
+		for(axis_index = 0; axis_index < 3; axis_index++){
+			rotate_angle[axis_index] = rotate_angle[axis_index] + (float)((ttt_initial[axis_index] + ttt[axis_index]) * Tsample / 2);
+		}
+
+		/*
+		*
+		*
+		* Compute magnitude of rotational angle summing over X and Y
+		* axis Rotation Rates.
+		*
+		* Convert from milli-degrees to degrees (Note that Rotation
+		* Rate is sampled in milli-degrees per second).
+		*
+		*/
+
+		angle_mag = 0;
+		for(axis_index = 0; axis_index < 3; axis_index++){
+			angle_mag = angle_mag + pow((rotate_angle[axis_index]), 2);
+		}
+
+		/*
+		* Compute angle magnitude and convert from milli-degrees to
+		* degrees
+		*/
+
+		angle_mag = sqrt(angle_mag)/1000;
+
+		/*
+		* Compute rotation angle magnitude
+		*/
+
+		if(angle_mag >= angle_mag_max_threshold){
+			break;
+		}
+
+	}
+
+	/*
+	* The loop above will exit under two conditions:
+	*
+	* 1) The SensorTile has not been moved over a period determined by
+	* MAX_ROTATION_ACQUIRE_CYCLES. Each loop cycle requires 10 milliseconds
+	* set by the integration interval of DATA_PERIOD_MS. Thus, this is a
+	* period of 4 seconds
+	* 2) The SensorTile has been moved to introduce an orientation angle
+	* change of greater than or equal to angle_mag_max_threshold.
+	*
+	* Thus, this system detects whether in State 1, there is a change
+	* in orientation after State 0 or there is no change in orientation.
+	*
+	* Assignment of features including for the third feature, *ttt_3.
+	*
+	* Consider a method similar to that of the Project of Tutorial 12
+	*
+	* Specifically, if angle_mag is greater than or equal to a threshold value
+	* then *ttt_3 is sent to the average of *ttt_1 and *ttt_2
+	*
+	* And if angle_mag is less than a threshold value, then *ttt_3 is set
+	* to zero
+	*
+	* Please also note that the loop above exits when angle_mag is equal to
+	* angle_mag_max_threshold. Therefore, angle_mag will not exceed this
+	* value. So, the threshold value for setting *ttt_3 should be chosen
+	* to be just less than angle_mag_max_threshold. Consider why this is
+	* required.
+	*/
+	/*
+	* **************************************************
+	* In the section below, replace the statement, *ttt_3 = 0; with a code
+	* segment including the conditional described in the comment above
+	*/
+
+	if(angle_mag >= angle_mag_max_threshold){
+		*ttt_3 = (*ttt_1 + *ttt_2) / 2;
+	}
+	else{
+		*ttt_3 = 0;
+	}
+
+	/*
+	* ***************************************************
+	*/
+
+	*ttt_mag_scale = (int)(angle_mag * 100);
+
+	sprintf(msg1, "\r\n");
+	CDC_Fill_Buffer((uint8_t*) msg1, strlen(msg1));
+
+	sprintf(msg1, "\r\nMotion with Angle Mag of %i degrees complete, Now Return to Next Start Position, ", (int)(angle_mag));
+	CDC_Fill_Buffer((uint8_t *) msg1, strlen(msg1));
+	BSP_LED_Off(LED1);
+	HAL_Delay(3000);
+	return;
+
+}
 
 void printOutput_ANN(ANN *net, int input_state, int * error) {
 
@@ -728,7 +923,7 @@ void TrainOrientation(void *handle, void *handle_g, ANN *net) {
 
 				Feature_Extraction_State_1(handle_g, &ttt_1, &ttt_2, &ttt_3,
 								&ttt_mag_scale);
-				Feature_Extraction_State_1(handle_g, &ttt_1, &ttt_2, &ttt_3,
+				Feature_Extraction_State_2(handle_g, &ttt_1, &ttt_2, &ttt_3,
 												&ttt_mag_scale);
 
 
@@ -744,7 +939,7 @@ void TrainOrientation(void *handle, void *handle_g, ANN *net) {
 
 				Feature_Extraction_State_1(handle_g, &ttt_1, &ttt_2, &ttt_3,
 								&ttt_mag_scale);
-				Feature_Extraction_State_1(handle_g, &ttt_1, &ttt_2, &ttt_3,
+				Feature_Extraction_State_2(handle_g, &ttt_1, &ttt_2, &ttt_3,
 												&ttt_mag_scale);
 
 				break;
@@ -758,7 +953,7 @@ void TrainOrientation(void *handle, void *handle_g, ANN *net) {
 
 				Feature_Extraction_State_1(handle_g, &ttt_1, &ttt_2, &ttt_3,
 								&ttt_mag_scale);
-				Feature_Extraction_State_1(handle_g, &ttt_1, &ttt_2, &ttt_3,
+				Feature_Extraction_State_2(handle_g, &ttt_1, &ttt_2, &ttt_3,
 												&ttt_mag_scale);
 
 				break;
@@ -772,7 +967,7 @@ void TrainOrientation(void *handle, void *handle_g, ANN *net) {
 
 				Feature_Extraction_State_1(handle_g, &ttt_1, &ttt_2, &ttt_3,
 								&ttt_mag_scale);
-				Feature_Extraction_State_1(handle_g, &ttt_1, &ttt_2, &ttt_3,
+				Feature_Extraction_State_2(handle_g, &ttt_1, &ttt_2, &ttt_3,
 												&ttt_mag_scale);
 
 				break;
@@ -786,7 +981,7 @@ void TrainOrientation(void *handle, void *handle_g, ANN *net) {
 
 				Feature_Extraction_State_1(handle_g, &ttt_1, &ttt_2, &ttt_3,
 								&ttt_mag_scale);
-				Feature_Extraction_State_1(handle_g, &ttt_1, &ttt_2, &ttt_3,
+				Feature_Extraction_State_2(handle_g, &ttt_1, &ttt_2, &ttt_3,
 												&ttt_mag_scale);
 
 				break;
@@ -800,7 +995,7 @@ void TrainOrientation(void *handle, void *handle_g, ANN *net) {
 
 				Feature_Extraction_State_1(handle_g, &ttt_1, &ttt_2, &ttt_3,
 								&ttt_mag_scale);
-				Feature_Extraction_State_1(handle_g, &ttt_1, &ttt_2, &ttt_3,
+				Feature_Extraction_State_2(handle_g, &ttt_1, &ttt_2, &ttt_3,
 												&ttt_mag_scale);
 
 				break;
@@ -987,7 +1182,7 @@ int Accel_Gyro_Sensor_Handler(void *handle, void *handle_g, ANN *net, int prev_l
 			Feature_Extraction_State_1(handle_g, &ttt_1, &ttt_2, &ttt_3,
 					&ttt_mag_scale);
 
-			Feature_Extraction_State_1(handle_g, &ttt_1, &ttt_2, &ttt_3,
+			Feature_Extraction_State_2(handle_g, &ttt_1, &ttt_2, &ttt_3,
 								&ttt_mag_scale);
 
 
@@ -1052,7 +1247,7 @@ int Accel_Gyro_Sensor_Handler(void *handle, void *handle_g, ANN *net, int prev_l
 				break;
 			case 5:
 				sprintf(msg1,
-						"\n\rNeural Network Classification - Orientation 6");
+						"\n\rNeural Network Classification - Motion 6");
 				CDC_Fill_Buffer((uint8_t *) msg1, strlen(msg1));
 				break;
 			case -1:
